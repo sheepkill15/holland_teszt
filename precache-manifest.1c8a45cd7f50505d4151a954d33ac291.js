6ca2b46334fa784590662c363d5984be58c41d01
sheepkill15 <simonarons15@gmail.com>
2020-10-19 11:20:26 +0300

    Updates

---
+++ precache-manifest.1c8a45cd7f50505d4151a954d33ac291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1793b31e3818989487d528400bc16fd9",
+    "url": "/holland_teszt/index.html"
+  },
+  {
+    "revision": "cc9f477be8e43835a591",
+    "url": "/holland_teszt/static/css/2.99a2b36c.chunk.css"
+  },
+  {
+    "revision": "1efe7021b8ea1ec00313",
+    "url": "/holland_teszt/static/css/main.65eddf35.chunk.css"
+  },
+  {
+    "revision": "cc9f477be8e43835a591",
+    "url": "/holland_teszt/static/js/2.70b8869a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/holland_teszt/static/js/2.70b8869a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efe7021b8ea1ec00313",
+    "url": "/holland_teszt/static/js/main.90893976.chunk.js"
+  },
+  {
+    "revision": "069108cd764f5a3f29ac",
+    "url": "/holland_teszt/static/js/runtime-main.d5376c22.js"
+  }
+]);
